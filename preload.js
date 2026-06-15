cf494a0a6070180704b571b88fb4904ed7b61113
davidraketic45-rgb <davidraketic45@gmail.com>
2026-06-15 09:28:51 +0530

    Delete preload.js

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronUpdater', {
-  onUpdateAvailable: (cb) => ipcRenderer.on('update-available', (_, version) => cb(version)),
-  onUpdateDownloaded: (cb) => ipcRenderer.on('update-downloaded', (_, version) => cb(version)),
-  onUpdateError: (cb) => ipcRenderer.on('update-error', (_, msg) => cb(msg)),
-  installUpdate: () => ipcRenderer.send('install-update'),
-});
